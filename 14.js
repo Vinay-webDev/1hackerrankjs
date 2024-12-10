a80b4d263b4362f78774278763a397c725e4f6bd
Vinay-webDev <justchatting3000@gmail.com>
2024-12-10 23:56:28 +0530

    wrong answer❌❌❌❌

--- 14.js
+++ 14.js
@@ -3,11 +3,12 @@
 const s = "Ab1";
 const s2 = "#Hackerrank";
 const s3 = "Mypassword12345678910";
-//🟢 it must have altease 6 length (password.length >= 6)
-//🟢 it must have atleast 1 lowercase ("abcdefghijklmnopqrstuvwxyz") ascii 97 - 122
-//🟢 it must have atleast 1 uppercase ("ABCDEFGHIJKLMNOPQRSTUVWXYZ") ascii 65 - 90;
-//🟢 it must have atleast 1 digit ("0123456789") ascii 48 to 57;
-//🟢 it must have atleast 1 special character ("!@#$%^&*()-+") ascii 33 to 45;
+const s4 = "E!%Z@";
+const s5 = "!%&&&*!%(&*^&^)(*^-)$&)#%@$*&(@(@$#))+#)+-!**^&(#!&^#@#*&)!%&^^+($@#!*)+$!-&!-()@$&+%@-)@$*@$+((@-^)";
+const s6 = "#17$9-4-66)07)5(7%)1!@8495(4*28)4!2(6!3%&+3517+41$4*112-&2)!23956&636)72)01&)33$7(!867!*728!$806+(#5";
+const s7 = "G0N1+93Gy0C!J4ECIc53+30O9az$K-TgDO^051y2+Qfvt94qI!k)lS(-8g65^A9mt%eRL5WP#f8R)i4O33j#&LNk6H%-pr-@d^*Z";
+const s8 = "0(27^9@";
+
 const strongPassword = (s) => {
     let asciiCount = 0;
     let lowercase = false;
@@ -36,6 +37,11 @@ const strongPassword = (s) => {
 console.log(strongPassword(s));   //3
 console.log(strongPassword(s2));  //1
 console.log(strongPassword(s3));  //1
+console.log(strongPassword(s4));  //2 wrong
+console.log(strongPassword(s5));  //3 wrong
+console.log(strongPassword(s6));  //2 wrong
+console.log(strongPassword(s7));  //0 wrong
+console.log(strongPassword(s8));  //2 wrong
 // this has few issues🫠🫠🫠
 
 
@@ -43,7 +49,11 @@ console.log(strongPassword(s3));  //1
 
 
 
-
+//🟢 it must have altease 6 length (password.length >= 6)
+//🟢 it must have atleast 1 lowercase ("abcdefghijklmnopqrstuvwxyz") ascii 97 - 122
+//🟢 it must have atleast 1 uppercase ("ABCDEFGHIJKLMNOPQRSTUVWXYZ") ascii 65 - 90;
+//🟢 it must have atleast 1 digit ("0123456789") ascii 48 to 57;
+//🟢 it must have atleast 1 special character ("!@#$%^&*()-+") ascii 33 to 45;
 
 
 
