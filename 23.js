63a1934b21e8ad1731ce379d3fb9cc2a10d7a5d1
Vinay-webDev <justchatting3000@gmail.com>
2024-12-14 13:38:29 +0530

    23

--- 23.js
+++ 23.js
@@ -1,23 +1,26 @@
 //valley
 const {steps1, path1} = {steps1: 8, path1: "DDUUUUDD"};
 const {steps2, path2} = {steps2: 8, path2: "UDDDUDUU"};
-const countValleys = (s, p) => {
+const {steps3, path3} = {steps3: 12, path3: "DDUUUUDDDDUU"};
+const countingValleys = (s, p) => {
     let count = 0;
     let seaLevel = 0;
-
     for (let i = 0; i < p.length; i++) {
         if (p[i] === "D") {
-            seaLevel++;
+            seaLevel--;
         }
         if (p[i] === "U") {
-            seaLevel--;
+            seaLevel++;
+        }
+        if (seaLevel === 0 && p[i] === "U") {
+            count++;
         }
     }
     return count;
 } 
+console.log(countingValleys(steps1, path1)); //1
+console.log(countingValleys(steps2, path2)); //1
+console.log(countingValleys(steps3, path3)); //2
 
 
 
-
-// console.log(countValleys(steps1, path1)); //1
-// console.log(countValleys(steps2, path2)); //2
