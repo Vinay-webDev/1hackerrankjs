3267313871022601d90572165ad310c0a5fb3654
Vinay-webDev <justchatting3000@gmail.com>
2024-12-23 16:33:48 +0530

    #32

---
+++ 32.js
@@ -0,0 +1,25 @@
+
+const arr1 = [10, 20, 20, 10, 10, 30, 50, 10, 20];
+const arr2 = [1, 2, 1, 2, 1, 3, 2];
+const sockMerchant = (arr) => {
+    let pair = 0;
+    const counts = arr.reduce((acc, value) => {
+        acc[value] = (acc[value] || 0) + 1;
+        return acc;
+    }, {});
+    Object.values(counts).forEach(value => {
+        pair += Math.floor(value / 2);
+    });
+    return pair;
+}
+console.log(sockMerchant(arr1));    //3
+console.log(sockMerchant(arr2));    //2
+
+
+
+
+
+
+
+
+
--- test.js
+++ test.js
@@ -21,17 +21,18 @@ const array = [2, 3, 4, 84, 100];
 //     console.log(true);
 // }//true
 
-const h = [1, 3, 4, 5, 5];
-const word = "abc";
-let hArr = [];
-const lexOrder = {
-    a: 0,b: 1,c: 2,d: 3,e: 4,f: 5,g: 6,h: 7,
-    i: 8,j: 9,k: 10,l: 11,m: 12,n: 13,o: 14,
-    p: 15,q: 16,r: 17,s: 18,t: 19,u: 20,v: 21,
-    w: 22,x: 23,y: 24,z: 25
-};
-// console.log(h[lexOrder[word[0]]]);
-for (let i = 0; i < word.length; i++) {
-    hArr.push(h[lexOrder.word[i]]);
-}
-console.log(hArr);
+// const h = [1, 3, 4, 5, 5];
+// const word = "abc";
+// let hArr = [];
+// const lexOrder = {
+//     a: 0,b: 1,c: 2,d: 3,e: 4,f: 5,g: 6,h: 7,
+//     i: 8,j: 9,k: 10,l: 11,m: 12,n: 13,o: 14,
+//     p: 15,q: 16,r: 17,s: 18,t: 19,u: 20,v: 21,
+//     w: 22,x: 23,y: 24,z: 25
+// };
+// // console.log(h[lexOrder[word[0]]]);
+// for (let i = 0; i < word.length; i++) {
+//     hArr.push(h[lexOrder.word[i]]);
+// }
+// console.log(hArr);
+console.log(Math.floor(1 / 2));
