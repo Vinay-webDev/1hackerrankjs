1d0d4a6e61569fd80ec28ca1750658a7f3c8753d
Vinay-webDev <justchatting3000@gmail.com>
2024-12-31 12:23:13 +0530

    #36

--- 36.py
+++ 36.py
@@ -5,19 +5,14 @@ n2, p2 = 5, 3
 n3, p3 = 6, 2
 class Solution:
     def pageCount(self, n, p):
-        ans = 0
         m = n // 2
-        if p == m:
-            ans = p // 2
-        elif p < m:
-            ans = (m - p) // 2
-        else:
-            ans = (p - m) // 2
-        return ans
+        start = p // 2
+        end = m - start
+        return min(start, end)
 sol = Solution()
 print(sol.pageCount(n1, p1)) #0
 print(sol.pageCount(n2, p2)) #1
-print(sol.pageCount(n3, p3)) #2
+print(sol.pageCount(n3, p3)) #1
 
 
 
