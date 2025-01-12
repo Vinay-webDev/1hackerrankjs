59ea0402b5ce9cff772bc1fcb595783d3062ec1b
Vinay-webDev <justchatting3000@gmail.com>
2025-01-12 18:14:46 +0530

    ❌#44❌

--- 44.py
+++ 44.py
@@ -1,21 +1,47 @@
 """MEDIUMs"""
 """Non-Divisible subset"""
+
+class Solution:
+    def nonDivisibleSubset(self, s, k):
+        arr = [*set(s)]
+        N = len(arr)
+        temp = []
+        for i in range(N):
+            if arr[i] % k != 0:
+                temp.append(arr[i])
+        N2 = len(temp)
+        res = []
+        for i in range(N2):
+            for j in range(i + 1, N2):
+                if (temp[i] + temp[j]) % k != 0:
+                    res.append(temp[i])    
+        return res
+# sol = Solution()
+# print(sol.nonDivisibleSubset(s1, k1))   #3
+# print(sol.nonDivisibleSubset(s2, k2))   #6
+#❌WRONG ANSWER❌
+""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
 s1, k1 = [1, 7, 2, 4], 3
 s2, k2 = [19, 10, 12, 24, 10, 25, 22], 4
 class Solution:
     def nonDivisibleSubset(self, s, k):
-        set_arr = [*set(s)]
-        N = len(set_arr)
-        temp = set()
-        for i in range(N):
-            for j in range(i+1, N):
-                if (set_arr[i] + set_arr[j]) % k != 0:
-                    temp.add(set_arr[i])
-                    temp.add(set_arr[j])
-        return len([*temp])
+        r = [num % k for num in s]
+        sett = set(r)
+        count = 0
+        print(r)
+        for remainder in r:
+            if (k - remainder) in sett:
+                count += 1
+        return count
 sol = Solution()
-print(sol.nonDivisibleSubset(s1, k1))   #3
-print(sol.nonDivisibleSubset(s2, k2))   #6
-#❌WRONG ANSWER❌
+print(sol.nonDivisibleSubset(s1, k1))   #0
+print(sol.nonDivisibleSubset(s2, k2))   #2
+#❌WRONG ANSWER❌           
+#huge logic issue😿
+
+
+
+
+
 
 
--- test.py
+++ test.py
@@ -1,2 +1,10 @@
-a = [2, 3, 2, 4, 3]
-print(a.index(3))
+# a = [2, 3, 2, 4, 3]
+# print(a.index(3))
+
+num = 10
+num2 = 1
+num3 = 5
+k = 3
+print(num % k)
+print(num2 % k)
+print(num3 % k)
