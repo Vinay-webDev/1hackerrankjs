25aade40f03774b235b0168ced8d3a6178f0c70b
Vinay-webDev <justchatting3000@gmail.com>
2025-01-13 02:00:48 +0530

    #49

--- 49.py
+++ 49.py
@@ -1,7 +1,5 @@
 """Jumping on the Clouds"""
-c1 = [0, 1, 0, 1, 0, 0, 0]
-c2 = [0, 0, 1, 0, 0, 1, 0]
-c3 = [0, 0, 0, 0, 1, 0]
+
 class Solution:
     def jumpingOnClouds(self, c):
         jumps = 0
@@ -15,6 +13,28 @@ class Solution:
                 jumps += 1
                 player += 1
         return jumps
-sol = Solution()
-print(sol.jumpingOnClouds(c1))
+# sol = Solution()
+# print(sol.jumpingOnClouds(c1))
 #⚠️Error index out of range⚠️
+""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+c1 = [0, 1, 0, 1, 0, 0, 0]
+c2 = [0, 0, 1, 0, 0, 1, 0]
+c3 = [0, 0, 0, 0, 1, 0]
+class Solution:
+    def jumpingOnClouds(self, c):
+        jumps = 0
+        player = 0
+        N = len(c)
+        while player < N - 1:
+            if player + 2 < N and c[player + 2] == 0:
+                player += 2
+            else:
+                player += 1
+            jumps += 1
+        return jumps
+sol = Solution()
+print(sol.jumpingOnClouds(c1))  #3
+print(sol.jumpingOnClouds(c2))  #4
+print(sol.jumpingOnClouds(c3))  #3
+
+
