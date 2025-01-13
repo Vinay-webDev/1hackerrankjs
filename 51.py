d039d4980aec9e034b031191fa79c4bf57dc82b3
Vinay-webDev <justchatting3000@gmail.com>
2025-01-14 02:01:18 +0530

    #51

--- 51.py
+++ 51.py
@@ -1,7 +1,7 @@
 """Jumping on the Clouds: Revisited"""
 c1, k1 = [0, 0, 1, 0, 0, 1, 1, 0], 2
 c2, k2 = [0, 0, 1, 0], 2
-c3, k3 = [0, 1, 0, 1, 0, 1, 0, 0, 1, 0, 0, 1, 0], 3
+# c3, k3 = [0, 1, 0, 1, 0, 1, 0, 0, 1, 0, 0, 1, 0], 3
 c4, k4 = [1, 1, 1, 0, 1, 1, 0, 0, 0, 0], 3
 class Solution:
     def jumpingOnClouds(self, c, k):
@@ -15,9 +15,43 @@ class Solution:
                 e -= 3
             player += k
         return e
+# sol = Solution()
+# print(sol.jumpingOnClouds(c1, k1))  #92
+# print(sol.jumpingOnClouds(c2, k2))  #96
+# print(sol.jumpingOnClouds(c3, k3))  #95
+# print(sol.jumpingOnClouds(c4, k4))  #95 ❌WRONG ANSWER❌
+#due to stupid problem description (constraints n % k = 0) ❌WRONG ANSWER❌
+""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+c1, k1 = [0, 0, 1, 0, 0, 1, 1, 0], 2
+c2, k2 = [0, 0, 1, 0], 2
+c4, k4 = [1, 1, 1, 0, 1, 1, 0, 0, 0, 0], 3
+class Solution:
+    def jumpingOnClouds(self, c, k):
+        N, player = len(c), 0
+        e = 100
+        while True:
+            player = (player + k) % N
+            e -= 1 + 2 * c[player]
+            if player == 0:
+                break
+        return e
 sol = Solution()
 print(sol.jumpingOnClouds(c1, k1))  #92
 print(sol.jumpingOnClouds(c2, k2))  #96
-print(sol.jumpingOnClouds(c3, k3))  #95
-print(sol.jumpingOnClouds(c4, k4))  #95 ❌WRONG ANSWER❌
-#due to stupid problem description (constraints n % k = 0) ❌WRONG ANSWER❌
+print(sol.jumpingOnClouds(c4, k4))  #80
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
