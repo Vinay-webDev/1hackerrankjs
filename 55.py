8602b215b3aa0d34959271781e30067f11fe52d8
Vinay-webDev <justchatting3000@gmail.com>
2025-01-20 01:15:16 +0530

    ⚠️#55⚠️
    I have a extra conditions which can avoid ig

---
+++ 55.py
@@ -0,0 +1,29 @@
+"""Making Anagrams"""
+s1, s2 = "abc", "amnop"
+class Solution:
+    def makingAnagram(self, s1, s2):
+        from collections import Counter
+        deletions = 0
+        s1_count = Counter(s1)
+        s2_count = Counter(s2) 
+        for i in range(len(s1)):
+            if s1[i] not in s2_count:
+                deletions += 1
+            else:
+                if s1_count[s1[i]] == s2_count[s1[i]]:
+                    continue
+                else:
+                    deletions += abs(s1_count[s1[i]] - s2_count[s1[i]])
+        for j in range(len(s2)):
+            if s2[j] not in s1_count:
+                deletions += 1
+            else:
+                if s2_count[s2[j]] == s2_count[s2[j]]:
+                    continue
+                else:
+                    deletions += abs(s2_count[s2[j]] - s2_count[s2[j]])
+        return deletions
+sol = Solution()
+print(sol.makingAnagram(s1, s2))
+#✅✅
+
