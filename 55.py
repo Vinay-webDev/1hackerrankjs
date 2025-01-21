b26078b629f26d8e8436626b2856711867407fb0
Vinay-webDev <justchatting3000@gmail.com>
2025-01-21 14:13:56 +0530

    #55

--- 55.py
+++ 55.py
@@ -1,7 +1,7 @@
 """Making Anagrams"""
 s1, s2 = "abc", "amnop"
 class Solution:
-    def makingAnagram(self, s1, s2):
+    def makingAnagram1(self, s1, s2):
         from collections import Counter
         deletions = 0
         s1_count = Counter(s1)
@@ -31,7 +31,7 @@ s1, s2 = "abc", "amnop"
 s3, s4 = "cde", "abc"
 s5, s6 = "absdjkvuahdakejfnfauhdsaavasdlkj", "djfladfhiawasdkjvalskufhafablsdkashlahdfa"
 class Solution:
-    def makingAnagram(self, s1, s2):
+    def makingAnagram2(self, s1, s2):
         from collections import Counter
         deletions = 0
         s1_count = Counter(s1)
@@ -49,15 +49,30 @@ class Solution:
                 if s2_count[s2[j]] != s1_count[s2[j]]:
                     deletions += abs(s2_count[s2[j]] - s1_count[s2[j]])
         return deletions
+# sol = Solution()
+# print(sol.makingAnagram(s1, s2))    #6
+# print(sol.makingAnagram(s3, s4))    #4
+# print(sol.makingAnagram(s5, s6))    #111❌❌
+#❌WRONG ANSWER❌
+""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""""
+s1, s2 = "abc", "amnop"
+s3, s4 = "cde", "abc"
+s5, s6 = "absdjkvuahdakejfnfauhdsaavasdlkj", "djfladfhiawasdkjvalskufhafablsdkashlahdfa"
+class Solution:
+    def makingAnagram(self, s1, s2):
+        import collections
+        s1_count = collections.Counter(s1)
+        s2_count = collections.Counter(s2)
+        unique_chars = set(s1).union(set(s2))
+        deletions = 0
+        for char in unique_chars:
+            deletions += abs(s1_count[char] - s2_count[char])
+        return deletions
 sol = Solution()
 print(sol.makingAnagram(s1, s2))    #6
 print(sol.makingAnagram(s3, s4))    #4
-print(sol.makingAnagram(s5, s6))    #111❌❌
-#❌WRONG ANSWER❌
-
-
-
-
+print(sol.makingAnagram(s5, s6))    #19
+#✅✅
 
 
 
